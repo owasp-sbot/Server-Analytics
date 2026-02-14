612678181e4039f921731e282206764a277fcc42
Dinis Cruz <dinis.cruz@owasp.org>
2026-02-14 19:32:08 +0000

    first commit

---
+++ tests/unit/utils/test_Version__Server_Analytics.py
@@ -0,0 +1,22 @@
+import server_analytics
+from unittest                          import TestCase
+from osbot_utils.utils.Files           import parent_folder, file_name
+from server_analytics.utils.Version    import version__server_analytics, Version
+
+
+class test_Version__Server_Analytics(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.version = Version()
+
+    def test_path_code_root(self):
+        assert self.version.path_code_root() == server_analytics.path
+
+    def test_path_version_file(self):
+        with self.version as _:
+            assert parent_folder(_.path_version_file()) == server_analytics.path
+            assert file_name    (_.path_version_file()) == 'version'
+
+    def test_value(self):
+        assert self.version.value() == version__server_analytics
